c6a10f080bc0576737c4e5e457ab4f78f60af9a5
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-29 06:47:09 +0530

    made changes

--- hello.c
+++ hello.c
@@ -1,7 +1,15 @@
+// 'C' is a compiled language : In order to execute it you need to compile it ie., make an executable file...
+// ... then execute that file.
+
+
 // The .h file is a header file in langs like 'c' and 'c++'. it contains declarations and definitions of elements...
 // ...such as functions, variables, macros and data types that are shared across multiple source files in a project.
 // 'stdio.h' file here, is a header file with standard input / output functions.
+
 #include <stdio.h>
+
+//'main()' function is the entry point of every 'c' program. it's where the main shit happens. 'int main()' means ...
+// ... the function returns an integer.
 int main() {
     printf("Hello, World!");
     return 0;
