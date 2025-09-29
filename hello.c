b3259a8a4800e0cc3c5c307bb89ffa41040b5347
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-29 06:38:36 +0530

    Hello Dawg

---
+++ hello.c
@@ -0,0 +1,8 @@
+// The .h file is a header file in langs like 'c' and 'c++'. it contains declarations and definitions of elements...
+// ...such as functions, variables, macros and data types that are shared across multiple source files in a project.
+// 'stdio.h' file here, is a header file with standard input / output functions.
+#include <stdio.h>
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
