8aea948003701e9a5e42d7728048271d3ca5c803
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-01 07:57:42 +0530

    added loops

---
+++ loops.c
@@ -0,0 +1,17 @@
+//and : &&
+//or  : ||
+//Not : !=
+//Eq  : ==
+
+#include <stdio.h>
+int main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+    if (age >= 18) {
+        printf("You are a major!\n");
+    } else if (age < 18) {
+        printf("You are a minor\n");
+    }
+    return 0;
+}
