92c2540009459b29c538d1e91ce4a06f3e3f7993
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-03 21:22:17 +0530

    made changes

--- input_output.c
+++ input_output.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
-    char name[10];
+    char *name[5];
     printf("Enter your name ...\n");
-    scanf("%s\n",name);
-    printf("Hi, %s!",name);
+    scanf("%s\n",&name);
+    printf("Hi, %s!",*name);
     return 0;
 }
