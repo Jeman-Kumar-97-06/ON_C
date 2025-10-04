f17830a216df10e9d2885408005be1681fd5e915
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-04 22:48:13 +0530

    made changes

--- input_output2.c
+++ input_output2.c
@@ -3,11 +3,15 @@ int main(){
     int integer;
     float floating;
     char name[100];
+    char grade;
+
     printf("Enter your age : ");
     scanf("%d",&integer);
     printf("Enter your percentage : ");
     scanf("%f",&floating);
     printf("Enter your name: ");
     scanf("%s",name);
+    printf("Enter your grad : ");
+    scanf("%c",&grade);
     printf("Your age is : %d\nYour percentage is : %f\nYour name is : %s\n",integer,floating,name);
 }
