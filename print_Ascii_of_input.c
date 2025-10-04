d4ec55e6c06bbc432c6322bbff8bb29732cbd7d5
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-04 22:58:25 +0530

    made changes

---
+++ print_Ascii_of_input.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    char x;
+    printf("Input the value for which you want to ASCII value : ");
+    scanf("%c",&x);
+    printf("ASCII Value of %c : %d\n",x,x);
+}
