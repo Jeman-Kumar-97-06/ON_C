82b737ae6c12577e04862842537f142bd0b664b0
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-05 07:26:51 +0530

    made changes

--- conditionals.c
+++ conditionals.c
@@ -6,12 +6,15 @@
 #include <stdio.h>
 int main(){
     int age;
-    printf("Enter your age: ");
+    printf("Give me your age: ");
     scanf("%d",&age);
-    if (age >= 18) {
-        printf("You are a major!\n");
-    } else if (age < 18) {
-        printf("You are a minor\n");
+    if(age >= 18) {
+        printf("You are a Major");
     }
-    return 0;
+    else if (age == 0) {
+        printf("You aren't born yet");
+    }
+    else {
+        printf("You are a minor");
+    }
 }
--- print_Ascii_of_input.c
+++ print_Ascii_of_input.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
     char x;
-    printf("Input the value for which you want to ASCII value : ");
+    printf("Input the value whose ASCII value you want : ");
     scanf("%c",&x);
     printf("ASCII Value of %c : %d\n",x,x);
+    return 0;
 }
