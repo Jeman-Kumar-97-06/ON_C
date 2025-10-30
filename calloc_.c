9effa20927d45237e9126c4422c4c94f22e0a314
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-30 18:32:16 +0530

    made changes

--- calloc_.c
+++ calloc_.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    
+    int number;
+    printf("Enter the number of player : ");
+    scanf("%d",&number);
+    int *scores = malloc(number * sizeof(int));
+    if (scores == NULL) {
+        printf("Sorry couldn't do it!");
+        return 1;
+    }
+
+    for (int i = 0; i< number; i++) {
+        printf("%d\n",scores[i]);
+    }
+    free(scores);
+    scores = NULL;
+
+    printf("\nCalloc start * \n");
+
+
+    int *scores_ = calloc(number,sizeof(int));
+    if (scores_ == NULL) {
+        printf("Sorry couldn't do it!");
+        return 1;
+    };
+
+    for (int i = 0; i< number; i++) {
+        printf("%d\n",scores_[i]);
+    }
+
+    free(scores_);
+    scores_ = NULL;
+
     return 0;
 }
