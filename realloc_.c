dd6d4384804130b63149cabfe3b89f47dcca9bb2
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-30 19:07:52 +0530

    made changes

---
+++ realloc_.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //realloc -> reallocation of memory size.
+    //           resize previously allocated memory.
+    //           realloc(pointer,bytes);
+    int number = 0;
+    
+    printf("Enter the number of grades : ");
+    scanf("%d",&number);
+
+    char *grades = malloc(number * sizeof(char));
+
+    if (grades == NULL) {
+        printf("Sorry!");
+        return 1;
+    }
+
+    for (int i = 0; i < number; i++) {
+        printf("Enter grad #%d : ",i+1);
+        scanf("%c\n",&grades[i]);
+    }
+
+    //Ask if user wants to change the size of the array, so that you can reallocate memory:
+    int newNumber = 0;
+    printf("Enter a new number : ");
+
+
+    //Print all the elements 
+    for (int i = 0; i < number; i++) {
+        printf("grade #%d : %c\n",i+1, grades[i]);
+    }
+
+    free(grades);
+    grades = NULL;
+
+    return 0;
+}
