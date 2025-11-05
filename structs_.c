92d6947e65d095d1efcca3c68c5de1e8b47b5d2d
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-11-05 22:16:39 +0530

    added structcode

--- structs_.c
+++ structs_.c
@@ -11,6 +11,21 @@ struct Student{
 int main(){
     struct Student student1 = {"SpongeBob",30,2.5,true};
     struct Student student2 = {"Jack",20,4.0,false};
+    struct Student student3;
+
+    printf("%s\n",student3.name);   //Garbage value
+    printf("%d\n",student3.age);    //Garbage value
+    printf("%.2f\n",student3.gpa);  //Garbage value
+    printf("%d\n",(student3.isFullTime) ? "Yes" : "No"); //Garbage Value: Random 'Yes' or "No"
+
+    struct Student student4 = {0}; //No garbage values like the above. All are set to '0's
+
+    strcpy(student4.name,"Sandy");
     
-    return 0;    
+
+    printf("%s\n",student1.name);
+    printf("%d\n",student1.age);
+    printf("%.2f\n",student1.gpa);
+    printf("%d\n",(student1.isFullTime) ? "Yes" : "No");
+    return 0;
 }
