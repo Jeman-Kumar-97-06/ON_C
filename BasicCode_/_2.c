74cec3855d279eeb347e7d2f43e53f95719a8a37
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-04-03 19:15:17 +0530

    made changes

---
+++ BasicCode_/_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Declare display() first: 
+void display();
+
+void main(){
+    printf("Hello World\n");
+    display();
+}
+
+//Defining display() here:
+void display(){
+    printf("Jeman\n");
+}
+
