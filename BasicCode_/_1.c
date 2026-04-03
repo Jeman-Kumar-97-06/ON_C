6285cce8d95bc1e6e5826a92b0d5cb97b8403aa0
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-04-03 15:59:40 +0530

    made changes;

---
+++ BasicCode_/_1.c
@@ -0,0 +1,9 @@
+// This is the Hello World Program in C:
+#include <stdio.h>
+
+//int main() -> returns a int --> put 'return 0' as the last line inside the function
+//void main() -> returns nothing --> No need for return statement.
+void main(){
+    printf("Hello World!\n");
+}
+
